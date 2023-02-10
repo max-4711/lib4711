9c2f3ccd3e9b2fd6432624cc49e963f290b1d187
max-4711 <commits-github@mail.studio-4711.com>
2023-02-10 18:18:02 +0100

    FIX: Make Lib4711.Blazor buildable

---
+++ Source/Lib4711.Blazor/Program.cs
@@ -0,0 +1,6 @@
+﻿class Program
+{
+    static void Main(string[] args) //We're building a library, so nothing to do here - but as the project build type is a blazor app, we need this to statisfy the compiler
+    {
+    }
+}
